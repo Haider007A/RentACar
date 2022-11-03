6c4b29dae10e931bcfa5db2621fb14f280f1d3a2
Haider007A <100359869+Haider007A@users.noreply.github.com>
2022-11-03 17:10:16 +0000

    Setting the BookingSystem class for getting rid from errors

--- src/ooc/yoursolution/BookingSystem.java
+++ src/ooc/yoursolution/BookingSystem.java
@@ -1,16 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ooc.yoursolution;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import ooc.enums.Make;
-import ooc.yoursolution.BookingSystemInterface;
-import ooc.yoursolution.RentACarInterface;
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 /**
  *
@@ -20,29 +18,30 @@ public class BookingSystem implements BookingSystemInterface{
 
     @Override
     public RentACarInterface setupRentACar(BufferedReader in) throws IOException {
-          //String name tells the name of company
-        String input, name;     
+        
+        // string name  tells the company name
+        String input, name;   
         
         //input tells the vehicle inside from the file
-        List<Car> cars = new ArrayList<>();  
+        List<Car> cars = new ArrayList<>();   
         name = in.readLine();
             
-        //Definer list splits the data substrings ":" and classify it as make,rate and price and quantity
-            while((input = in.readLine())!=null){        
+         //define list splits the data of substrings ":" and set it as make,rate,price and quantity
+            while((input = in.readLine())!=null){       
                 String definer[] = input.split(":");
                 
-                //Use positional indexes in java to define various objects
-                Make make = Make.valueOf(definer[0]);         
+                 //use of positional indexes to define various objects
+                Make make = Make.valueOf(definer[0]);        
                 double rate = Double.parseDouble(definer[1]);
                 int numOfCars = Integer.parseInt(definer[2]);
                 
-                //a simmple for loop fills in the list with different cars and quantity
+                //this simmple method for loop fills in the list with different cars and quantity
                 for(int i=0;i<numOfCars;i++){       
                     Car car = new Car(i,make,rate);
                     cars.add(car);
                 }
             
-        }       //returns the instance of RentACar class
+        }       //this will return the instance of RentACar class
             RentACarInterface rentACarInterface = new RentACar(cars,name);
             
             return rentACarInterface;
@@ -50,3 +49,4 @@ public class BookingSystem implements BookingSystemInterface{
 
 }
     
+
